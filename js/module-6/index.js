be529fd2663f51b266fc9163d231748dbc63f808
Suyash Singh <0808cs211182.ies@ipsacademy.org>
2025-06-18 11:58:23 +0530

    Completed-module-6-Task To change the background color of the second column of a table having “n ” rows on click by using querySelector.

---
+++ js/module-6/index.js
@@ -0,0 +1,11 @@
+// Javascript Task:Change the background color of the second column of a table having “n ” rows on click by using querySelector.
+
+const table = document.querySelector("table");
+const secondColumnCells = table.querySelectorAll("tr td:nth-child(2)");
+console.log(secondColumnCells);
+
+secondColumnCells.forEach((cell) => {
+  cell.addEventListener("click", function () {
+    cell.style.backgroundColor = "lightblue";
+  });
+});
